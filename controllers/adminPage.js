bebc03f2de7515b1df6c21b2f3232479de0cc2b7
Stephen Brooks <codedev1980@gmail.com>
2023-02-17 14:56:37 -0800

    update

--- controllers/adminPage.js
+++ controllers/adminPage.js
@@ -5,11 +5,11 @@ const CaliforniaDB = require('../models/CaliforniaPost');
 const UsersDB = require('../models/Users');
 
 module.exports = async (req, res) => {
-    const nevada = await NevadaDB.find({}).limit(3).sort({_id: -1}).populate('userid');
-    const hawaii = await ArizonaDB.find({}).limit(3).sort({_id: -1}).populate('userid');
-    const arizona = await HawaiiDB.find({}).limit(3).sort({_id: -1}).populate('userid');
-    const california = await CaliforniaDB.find({}).limit(3).sort({_id: -1}).populate('userid');
-    const user = await UsersDB.find({}).limit(3).sort({_id: -1});
+    const nevada = await NevadaDB.find({}).limit(1).sort({_id: -1}).populate('userid');
+    const hawaii = await HawaiiDB.find({}).limit(1).sort({_id: -1}).populate('userid');
+    const arizona = await ArizonaDB.find({}).limit(1).sort({_id: -1}).populate('userid');
+    const california = await CaliforniaDB.find({}).limit(1).sort({_id: -1}).populate('userid');
+    const user = await UsersDB.find({}).limit(1).sort({_id: -1});
 
     res.render('staffAccount', {
         title: "Metal-Weld Staff",
--- controllers/arizonaInfoPost.js
+++ controllers/arizonaInfoPost.js
@@ -1,9 +1,9 @@
 const InfoPost = require('../models/ArizonaPost');
 
 module.exports = async (req, res) => {
-    const arizona = await InfoPost.findById(req.params.id).populate('userid');
+    const arizonaInfo = await InfoPost.findById(req.params.id).populate('userid');
     res.render('singleArizona', {
         title: "Arizona Post Information",
-        arizona
+        arizonaInfo
     })
 }
