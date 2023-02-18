ce9ead8e6d2eb48e8285fd4b49d948791225fdc1
Stephen Brooks <codedev1980@gmail.com>
2023-02-18 11:46:20 -0800

    update

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -2,6 +2,6 @@
 module.exports = async (req, res) => {
 
     res.render('index', {
-        title: "Metal-Weld Specialties - America's Most Trust Fabricator"
+        title: "Metal-Weld Specialties - America's Most Experienced & Trusted Fabricator"
     })
 }
