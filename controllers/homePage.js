aba3aa4c1313de799f321662a410eb0010f871c6
CodeDev1980 <98498887+CodeDev1980@users.noreply.github.com>
2023-02-19 08:42:38 -0600

    Update

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,8 +1,9 @@
 const Users = require('../models/Users');
 
 module.exports = async (req, res) => {
-
+    const userAdmin = await Users.find().limit(1).sort({_id: -1}).populate('userid');
     res.render('index', {
         title: "Metal-Weld Specialties - America's Most Experienced & Trusted Fabricator"
-    })
+        userAdmin
+     })
 }
